09125a45c88ebbd9a86d53d7aa9e5412965d8ca3
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-16 17:19:47 +0700

    Membuat perulangan while

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-//declare variable global
+//Deklarasi variable global
 int element[10];
 int nPanjang;
 int x;
+
+//Membuat fungsi untuk menginput data ke dalam array
+void input()
+{
+    //Membuat perulangan while
+    while (true)
+    {
+       
+    }
+
+    
+}
