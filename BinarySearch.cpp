7dcbd271a2f12ebdea5e75696224fe7bc7cfa982
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-16 17:22:33 +0700

    Membuat kondisi jika jumlah elemen tidak lebih dari 10, maka perulangan akan berhenti

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -12,8 +12,18 @@ void input()
     //Membuat perulangan while
     while (true)
     {
-       
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)//Membuat kondisi jika jumlah elemen tidak lebih dari 10, maka perulangan akan berhenti
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
     }
 
-    
+   
 }
+
